6c77623c3a8dcb1578e4d3e91948d04077f03951
Gerardo Monaco <monacogj@gmail.com>
2025-01-14 20:48:43 -0300

    segundo commit

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -3,5 +3,27 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Hola <b>Rocio Ailen Monaco</b>, espero que tengas una amena jornada de trabajo junto a tu hermano Gerardo.<br>
+    Te cuento que Gerardo es <b>muy lento</b> pero no obstante hizo la tarea que le indicaste--> Ver los videos.";
+});
+
+
+Route::get('/hermano/{nombre}', function ($nombre) {
+
+    $contenido = "";
+
+    switch ($nombre) {
+
+        case "rocio":
+            $contenido = "Rocio, es la hermana preferia de Gerardo. Es una gran persona , inteligente a la cual admira profundamente.";
+            break;
+        case "vanesa":
+            $contenido = "Vanesa, es la 2da hermana en preferencia de Gerardo. Les rebalo a los hermanos un sobrino hermoso llamado Benjamin.";
+            break;
+        default:
+            $contenido = "Gerardo no tiene un hermano/a llamada/o {} ";
+    }
+
+
+    return $contenido;
 });
